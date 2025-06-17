45e01e4527fc26b019fa761601fb5e9425bd751e
jiwon <anna323532353235@gmail.com>
2025-06-17 17:45:10 +0900

    <create>naverbook

--- src/pages/NaverBook.jsx
+++ src/pages/NaverBook.jsx
@@ -1,3 +1,21 @@
-const NaverBook = () => {};
-// sss
+const NaverBook = () => {
+  return (
+    <div className="naverBook">
+      <div className="bigImg">
+        <img src="#"></img>
+      </div>
+      <div className="smallImg">
+        <img src="#"></img>
+        <img src="#"></img>
+        <img src="#"></img>
+        <img src="#"></img>
+      </div>
+      <div>
+        <h4>해봐you의원</h4>
+        <p>방문자 리뷰 </p>
+      </div>
+    </div>
+  );
+};
+
 export default NaverBook;
