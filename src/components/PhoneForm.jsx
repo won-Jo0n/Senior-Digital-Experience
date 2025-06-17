f8af4ff2339f9fed14263bd969631afff6836c23
jiwon <anna323532353235@gmail.com>
2025-06-17 15:06:26 +0900

    <create>phoneForm

---
+++ src/components/PhoneForm.jsx
@@ -0,0 +1,14 @@
+import "./PhoneForm.css";
+
+const PhoneForm = () => {
+  return (
+    <div className="PhoneForm">
+      <Header />
+      <div className="PhoneImg">
+        <img src="#"></img>
+      </div>
+    </div>
+  );
+};
+
+export default PhoneForm;
