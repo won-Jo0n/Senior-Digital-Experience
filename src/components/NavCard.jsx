ea3d190d91eb86396a128fc3e852d8de09ff2d49
wonJoon <jee861203@gmail.com>
2025-06-18 17:13:20 +0900

    no message

--- src/components/NavCard.jsx
+++ src/components/NavCard.jsx
@@ -1,10 +1,14 @@
 import "./NavCard.css";
 import { getNavCardImage } from "../util/getNavCardImage";
 
-const NavCard = ({ title, explanation, cardImage }) => {
+const NavCard = ({ title, explanation, cardImage, onClick }) => {
   return (
     <div className="NavCard">
-      <div>
+      <div
+        onClick={() => {
+          onClick();
+        }}
+      >
         <img className="NavCardImage" src={getNavCardImage(cardImage)} />
         <h4 className="title">{title}</h4>
         <div className="explanation">{explanation}</div>
