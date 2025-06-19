480332d6533a5255b7e5890cc426e65e5f3b7261
jiwon <anna323532353235@gmail.com>
2025-06-19 09:05:08 +0900

    <modify> Edit naverbook content

--- src/pages/NaverBook.jsx
+++ src/pages/NaverBook.jsx
@@ -1,8 +1,8 @@
 import { Routes, Route } from "react-router-dom";
 import NaverBook_page01 from "../components/NaverBookPages/NaverBook_page01";
 import NaverBook_page02 from "../components/NaverBookPages/NaverBook_page02";
-import NaverBook_page03 from "../components/NaverBookPages/NaverBook_page03";
-import NaverBook_page04 from "../components/NaverBookPages/NaverBook_page04";
+// import NaverBook_page03 from "../components/NaverBookPages/NaverBook_page03";
+// import NaverBook_page04 from "../components/NaverBookPages/NaverBook_page04";
 
 const NaverBook = () => {
   return (
@@ -12,8 +12,8 @@ const NaverBook = () => {
         <Route path="/" element={<NaverBook_page01 />} />
         <Route path="page01" element={<NaverBook_page01 />} />
         <Route path="page02" element={<NaverBook_page02 />} />
-        <Route path="page03" element={<NaverBook_page03 />} />
-        <Route path="page04" element={<NaverBook_page04 />} />
+        {/* <Route path="page03" element={<NaverBook_page03 />} />
+        <Route path="page04" element={<NaverBook_page04 />} /> */}
 
         <Route path="/*" element={<NotFound />} />
       </Routes>
