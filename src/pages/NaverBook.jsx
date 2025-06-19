26709af5f66b520ae0b3ba74f6dbbf72817969a4
jiwon <anna323532353235@gmail.com>
2025-06-19 10:04:52 +0900

    <modify> Edit naverbookpages

--- src/pages/NaverBook.jsx
+++ src/pages/NaverBook.jsx
@@ -1,6 +1,6 @@
 import { Routes, Route } from "react-router-dom";
-import NaverBook_page01 from "../pages/NaverBookPages/NaverBook_page01";
-import NaverBook_page02 from "../pages/NaverBookPages/NaverBook_page02";
+import NaverBook_page01 from "../components/NaverBookPages/NaverBook_page01";
+import NaverBook_page02 from "../components/NaverBookPages/NaverBook_page02";
 // import NaverBook_page03 from "../components/NaverBookPages/NaverBook_page03";
 // import NaverBook_page04 from "../components/NaverBookPages/NaverBook_page04";
 
@@ -9,8 +9,8 @@ const NaverBook = () => {
     <div>
       <h4>hi</h4>
       <Routes>
-        <Route path="/" element={<NaverBook_page01 />} />
         <Route path="/page01" element={<NaverBook_page01 />} />
+        {/* <Route path="/page01" element={<NaverBook_page01 />} /> */}
         <Route path="/page02" element={<NaverBook_page02 />} />
         {/* <Route path="page03" element={<NaverBook_page03 />} />
         <Route path="page04" element={<NaverBook_page04 />} /> */}
@@ -22,3 +22,4 @@ const NaverBook = () => {
 };
 
 export default NaverBook;
+
