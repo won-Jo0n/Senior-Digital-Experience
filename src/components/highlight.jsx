484e0bc70ad8eabc3c91e05adc121f643516720b
jiwon <anna323532353235@gmail.com>
2025-06-24 14:34:00 +0900

    <feat> modify highlight css

--- src/components/highlight.jsx
+++ src/components/highlight.jsx
@@ -5,7 +5,7 @@ const Highlight = ({ children, tooltip, color }) => {
   return (
     <div className="highlight-outer">
       {tooltip && <div className={`tooltip tooltip-${color}`}>{tooltip}</div>}
-      <div className={"highlight"}>{children}</div>
+      <div className={`highlight highlight-${color}`}>{children}</div>
     </div>
   );
 };
