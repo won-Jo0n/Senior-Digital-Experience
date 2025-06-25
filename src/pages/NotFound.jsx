f9abfa92ca757a52a52d80866f19a8c6f443bb23
jiwon <anna323532353235@gmail.com>
2025-06-25 11:18:28 +0900

    <create> add not found page

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,5 +1,28 @@
+import { useNavigate } from "react-router-dom";
+import "./NotFound.css";
+
 const NotFound = () => {
-  return <div>NotFound</div>;
+  const navigate = useNavigate();
+
+  return (
+    <div className="notfound-container">
+      <h1 className="error-title">404</h1>
+      <p className="error-subtitle">이런! 페이지를 찾을 수 없습니다.</p>
+
+      <div className="image-wrapper">
+        <img src="/limhero.png" alt="임영웅" />
+        <button onClick={() => navigate("/")} className="limheroBtn">
+          홈으로
+          <br />
+          돌아가기
+        </button>
+      </div>
+
+      <p className="caption">
+        버튼을 누르시면 홈으로 이동해요 <b>건행</b>~ 💚
+      </p>
+    </div>
+  );
 };
 
 export default NotFound;
